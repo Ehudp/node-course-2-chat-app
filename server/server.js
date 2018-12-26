cc351fb50f14561a7df447fb9b7fc3e973cc571a
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-12-26 13:10:15 +0200

    emite new message

--- server/server.js
+++ server/server.js
@@ -12,20 +12,20 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-
     console.log('New user connected');
-    socket.emit('newMessage', {
-        from: 'server1@test.com',
-        text: 'Hi client',
-        createdAt: 123
-    });
 
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
         console.log('User was disconnected');
+
     });
 });
 
