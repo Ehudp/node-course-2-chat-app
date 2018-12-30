8fab946707ca8f83f9f1a5c52af08a349559044f
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-12-30 15:55:54 +0200

    add validetors

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,8 +19,16 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
+    var params = jQuery.deparam(window.location.search);
 
-    console.log('Connected to server');
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        } else {
+            console.log('No Error');
+        }
+    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const { isRealString } = require('./utils/validation');
+
 var { generateMessage, generateLocationMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -17,6 +19,13 @@ io.on('connection', (socket) => {
     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User joined'));
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.room)) {
+            callback('Name and room are required')
+        } else {
+            callback();
+        }
+    });
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
